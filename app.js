0377081258a4ccc88d8dd2e238407b2331b619f1
gcamuk <duncanritchie@btinternet.com>
2019-06-13 09:38:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const field = document.querySelectorAll('.field');
+const scoreBoard = document.querySelector('.score');
+const mrbump = document.querySelectorAll('.mrbump');
+
+
+
+// let mrBump = document.getElementsByClassName("mrbump").src = "assets/mrbump.png";
+// let field = document.getElementsByClassName("field").src = "assets/field.png";
+
+
+
